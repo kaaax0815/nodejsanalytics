a59f817182e7c0e6c330f5b6ad9f746002546f88
kaaax0815 <999999bst@gmail.com>
2021-03-17 22:09:33 +0100

    📦 Please trigger a release 1.0.0

--- release.config.js
+++ release.config.js
@@ -1,2 +1,1 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-module.exports = require('@betahuhn/config').releaseMaster;
+module.exports = require('@betahuhn/config').releaseMaster
