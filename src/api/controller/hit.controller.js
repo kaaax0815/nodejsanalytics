1bca6e619bd705771a323502195435fcaf0c2e15
kaaax0815 <999999bst@gmail.com>
2021-03-17 21:03:10 +0100

    ✨ Working Hits Feature

--- src/api/controller/hit.controller.js
+++ src/api/controller/hit.controller.js
@@ -2,13 +2,17 @@ const httpStatus = require('http-status');
 const Hit = require('../models/hit.model');
 
 /**
- * Create new user
+ * Update Hit if new day create new Schema
  * @public
  */
-exports.hit = (req, res) => {
+exports.hit = async (req, res) => {
   try {
-    res.json({ status: '200' });
+    const currentdate = new Date().toISOString().slice(0, 10);
+    await Hit.updateOne({ date: currentdate }, { $inc: { hits: +1 } }, { upsert: true });
+    res.status(httpStatus.OK);
+    res.json({ status: 200 });
   } catch (error) {
+    console.log(error);
     res.status(httpStatus.INTERNAL_SERVER_ERROR);
     res.json({ status: '500' });
   }
