75de5adee8914f9d18d759d2bafa313cf8bc53b9
kaaax0815 <999999bst@gmail.com>
2021-03-18 18:19:52 +0100

    ✅ Unit Tests

--- src/api/tests/unit/hits.test.js
+++ src/api/tests/unit/hits.test.js
@@ -1,8 +1,10 @@
 const request = require('supertest');
 const app = require('../../../index');
 
-it('should respond with 200', () => request(app)
-  .post('/v1/fortnitepy/hit')
-  .send()
-  .expect(200)
-  .expect({ status: 200 }));
+describe('fortnitepy hits', () => {
+  it('should respond with 200', () => request(app)
+    .post('/v1/fortnitepy/hit')
+    .send()
+    .expect(200)
+    .expect({ status: 200 }));
+});
--- src/api/tests/unit/status.test.js
+++ src/api/tests/unit/status.test.js
@@ -1,26 +1,28 @@
 const request = require('supertest');
 const app = require('../../../index');
 
-it('should respond with 200', () => request(app)
-  .post('/v1/status')
-  .send()
-  .expect(200)
-  .expect({ status: 'OK' }));
+describe('Status responses', () => {
+  it('should respond with 200', () => request(app)
+    .post('/v1/status')
+    .send()
+    .expect(200)
+    .expect({ status: 'OK' }));
 
-it('should respond with 200', () => request(app)
-  .get('/v1/status')
-  .send()
-  .expect(200)
-  .expect({ status: 'OK' }));
+  it('should respond with 200', () => request(app)
+    .get('/v1/status')
+    .send()
+    .expect(200)
+    .expect({ status: 'OK' }));
 
-it('should respond with 200', () => request(app)
-  .post('/v1/fortnitepy/status')
-  .send()
-  .expect(200)
-  .expect({ status: 'fortnitepy OK' }));
+  it('should respond with 200', () => request(app)
+    .post('/v1/fortnitepy/status')
+    .send()
+    .expect(200)
+    .expect({ status: 'fortnitepy OK' }));
 
-it('should respond with 200', () => request(app)
-  .get('/v1/fortnitepy/status')
-  .send()
-  .expect(200)
-  .expect({ status: 'fortnitepy OK' }));
+  it('should respond with 200', () => request(app)
+    .get('/v1/fortnitepy/status')
+    .send()
+    .expect(200)
+    .expect({ status: 'fortnitepy OK' }));
+});
